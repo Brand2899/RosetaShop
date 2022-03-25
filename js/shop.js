a69d819525fb985718a95dd3858966c8bc988828
Alejandro <alejandrobrand28@gmail.com>
2022-03-25 15:20:57 -0500

    applying sass to shop page

--- js/shop.js
+++ js/shop.js
@@ -1,10 +1,3 @@
-let user = {
-    user_name: "user",
-    user_email: "user@email.com",
-    user_age: 18,
-    user_gender: "F",
-}
-
 const stock = [
     {
         obj_name: "Selini",
@@ -59,6 +52,39 @@ const stock = [
         obj_reference: "C1465476",
         obj_color: "Blanco",
         obj_isPrinted: false
+    },
+
+    {
+        obj_name: "Nairobi",
+        obj_price: 90000,
+        obj_img: "./img/img_products_placeholder.jpg",
+        obj_category: "Pantalon",
+        obj_colection: "Nombres",
+        obj_reference: "F4896156",
+        obj_color: "Beige",
+        obj_isPrinted: false
+    },
+
+    {
+        obj_name: "Rubí",
+        obj_price: 150000,
+        obj_img: "./img/img_products_placeholder.jpg",
+        obj_category: "Outfit",
+        obj_colection: "Piedas Preciosas",
+        obj_reference: "D1767146",
+        obj_color: "Verde Menta",
+        obj_isPrinted: false
+    },
+
+    {
+        obj_name: "Milan",
+        obj_price: 120000,
+        obj_img: "./img/img_products_placeholder.jpg",
+        obj_category: "Vestido",
+        obj_colection: "Ciudades",
+        obj_reference: "C1465476",
+        obj_color: "Blanco",
+        obj_isPrinted: false
     }
 ];
 
@@ -78,23 +104,3 @@ stock.forEach( product => {
     `;
     stockPrint.appendChild(individualProduct);
 });
-
-
-/*
-const { user_age, user_email, user_gender, user_name } = user;
-
-stock.forEach( product => {
-    const {obj_name: name, obj_price: price, obj_img: img, obj_category: category, obj_colection: colection, obj_reference: reference, obj_color: color, obj_isPrinted: isPrinted} = product;
-
-    var printList = document.getElementById("printList");
-    var line = document.createElement("li");
-
-    if(!isPrinted && price >= 90000){
-        
-        var content = document.createTextNode(name + " tiene un precio de: $ " + price);
-
-        printList.appendChild(line);
-        line.appendChild(content);
-    }
-})
-*/
