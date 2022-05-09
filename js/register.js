914b87eaccc6b6235c7630642dcde9c6b33937c8
Alejandro <alejandrobrand28@gmail.com>
2022-05-09 02:36:49 -0500

    creation of login and register separate login

--- js/register.js
+++ js/register.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { async } from "@firebase/util";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+import { createUser } from "./scripts/registerScript";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -28,22 +28,11 @@ registerForm.addEventListener("submit", e =>{
     const email = registerForm.user_email.value;
     const password = registerForm.user_password.value;
 
-    createUser(name, email, password);
-});
-
-async function createUser(name, email, password){
-    try{
-        const { user } = await createUserWithEmailAndPassword(auth, email, password);
-        alert(`Bienvenido, usuario ${user.uid}`);
-
-    } catch(e){
-        if(e.code === "auth/weak-password"){
-            alert("Tu contraseña debe tener al menos 6 caracteres")   
-        }
-
-        if(e.code === "auth/email-already-in-use"){
-            alert("Tu correo ya está en uso")   
-        }
-        console.log(e.code);
+    const newUser = {
+        name,
+        email,
+        password
     }
-}
+
+    createUser(auth, newUser);
+});
